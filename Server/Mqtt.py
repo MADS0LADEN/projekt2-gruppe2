3a044613d7474b43cd3939835eee3f8dd9fc3b3a
JKitstudent <10503138@ucn.dk>
2024-05-09 13:26:38 +0200

    OOP

--- Server/Mqtt.py
+++ Server/Mqtt.py
@@ -1,24 +1,30 @@
 import paho.mqtt.client as mqtt
 
-# Angiv MQTT-brokerens adresse
-mqtt_broker = "192.168.15.24"  # Du skal muligvis ændre dette til den faktiske adresse på din broker
-mqtt_port = 1883
+class modtageData:
+    def __init__(self):
+        self.data = None
+        self.mqtt_broker = "192.168.15.24" #adressen på brokeren
+        self.mqtt_port = 1883
 
-# Denne funktion udføres, når der modtages en besked fra brokeren
-def on_message(client, userdata, message):
-    print(f"Modtaget besked på emne '{message.topic}': {str(message.payload.decode('utf-8'))}")
 
-# Opret en MQTT-klient og tilslut til brokeren
-print("Hej")
-client = mqtt.Client()
-client.connect(mqtt_broker, mqtt_port)
+    def start_server(self):
+        # Denne funktion udføres, når der modtages en besked fra brokeren
+        def on_message(client, userdata, message):
+            print(f"Modtaget besked på emne '{message.topic}': {str(message.payload.decode('utf-8'))}")
 
-# Angiv funktionen til at håndtere modtagelse af beskeder
-client.on_message = on_message
+        # Opret en MQTT-klient og tilslut til brokeren
+        print("Hej")
+        client = mqtt.Client()
+        client.connect(self.mqtt_broker, self.mqtt_port)
 
-# Abonner på det ønskede emne
-client.subscribe("test")
+        # Angiv funktionen til at håndtere modtagelse af beskeder
+        client.on_message = on_message
 
-# Start loopet for at modtage beskeder
-print("Server startet")
-client.loop_forever()
+        # Abonner på det ønskede emne
+        client.subscribe("test")
+
+        # Start loopet for at modtage beskeder
+        print("Server startet")
+        client.loop_forever()
+
+class sendeData:
